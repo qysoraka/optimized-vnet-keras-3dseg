8c193857cfd7d078583d5300ab7801a2e306a43f
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-07-12 17:38:07 +0000

    contacto

--- run_vnet3d.py
+++ run_vnet3d.py
@@ -9,3 +9,8 @@ if __name__ == '__main__':
     from keras.optimizers import Adam, SGD
     from utils import DataGenerator, dice_loss, dice_coefficient, ModelAndWeightsCheckpoint
     from vnet3d import VNet
+    from keras.callbacks import LearningRateScheduler, Callback, TensorBoard, EarlyStopping
+    
+    parser = argparse.ArgumentParser(description="Script to run UNet3D")
+    parser.add_argument('--core_tag', '-ct', required=True)
+    parser.add_argument('--nii_dir', '-I', required=True)

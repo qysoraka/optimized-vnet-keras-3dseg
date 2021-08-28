b6d4a2fe8daceaa83234ffaaa05ced0b845f932d
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-08-28 11:01:14 +0000

    more merging

--- run_vnet3d.py
+++ run_vnet3d.py
@@ -136,3 +136,16 @@ if __name__ == '__main__':
     
     # [V-Net 3D] # Fix #6: n_in=2 --> 4
     model = VNet(image_shape=image_shape, n_in=4, n_out=3, 
+        strides=1, padding='same', kernel_size=5,
+        groups=args.group_size, data_format='channels_first',
+        filters=args.f_root)
+    if args.optimizer == 'adam':
+        optimizer = Adam(lr = args.learning_rate) # FIX #2
+    elif args.optimizer == 'sgd':
+        optimizer = SGD(lr=args.learning_rate, decay=1e-6, momentum=0.99)
+    else:
+        raise Exception('[ERROR] args.optimizer = {}'.format(args.optimizer))
+    
+    if len(existing_models) > 0: # if saved model exists
+        print(existing_models)
+        best_model = existing_models[0] # sorted ix 0 has lowest vl

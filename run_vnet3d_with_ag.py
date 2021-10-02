3bda111d6718c6d6698f890c382e35fbcde7e076
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-10-02 19:52:03 +0000

    cheatsheets

--- run_vnet3d_with_ag.py
+++ run_vnet3d_with_ag.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
     import argparse
     import time, re
     import tensorflow as tf
+    from keras.optimizers import Adam, SGD
+    from utils import DataGenerator, dice_loss, dice_coefficient, ModelAndWeightsCheckpoint
+    from vnet3d import VNet
+    from keras.callbacks import LearningRateScheduler, Callback, TensorBoard, EarlyStopping
+    
+    parser = argparse.ArgumentParser(description="Script to run UNet3D")
+    parser.add_argument('--core_tag', '-ct', required=True)
+    parser.add_argument('--nii_dir', '-I', required=True)

60a200a5cfc6974fd25bda0112da5a35a1105899
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-10-22 17:40:35 +0000

    da

--- run_vnet3d_with_ag.py
+++ run_vnet3d_with_ag.py
@@ -103,3 +103,13 @@ if __name__ == '__main__':
         patience=15, verbose=1, mode='max', baseline=None, 
         restore_best_weights=True)
     time_tag = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    tf_log_dir = '{}/logs/vnet'.format(cloud_dir)
+    try:
+        os.system('mkdir -p ' + tf_log_dir)
+    except:
+        pass
+    if not os.path.exists(tf_log_dir):
+        raise Exception("{} does not exist".format(tf_log_dir))
+    log_dir = os.path.join(tf_log_dir, args.core_tag + '_' + time_tag)
+    tensorboard_cb = TensorBoard(log_dir=log_dir)
+    

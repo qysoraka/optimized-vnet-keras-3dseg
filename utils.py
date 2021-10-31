9e3528176ae5bae3f5bbe5714eb368623bd8eb00
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-10-31 19:28:33 +0000

    Updated Flood sensors

--- utils.py
+++ utils.py
@@ -14,3 +14,15 @@ from scipy.ndimage import zoom
 from keras import backend as K
 from keras.callbacks import Callback
 
+
+class ModelAndWeightsCheckpoint(Callback):
+    """Save the model after every epoch.
+    `filepath` can contain named formatting options,
+    which will be filled with the values of `epoch` and
+    keys in `logs` (passed in `on_epoch_end`).
+    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
+    then the model checkpoints will be saved with the epoch number and
+    the validation loss in the filename.
+    # Arguments
+        filepath: string, path to save the model file.
+        monitor: quantity to monitor.

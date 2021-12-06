6ecaee7fcbf81bf44d6387973b143232059a4887
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-12-06 05:38:49 +0000

    update name

--- utils.py
+++ utils.py
@@ -79,3 +79,26 @@ class ModelAndWeightsCheckpoint(Callback):
         if self.epochs_since_last_save >= self.period:
             self.epochs_since_last_save = 0
             filepath = self.filepath.format(epoch=epoch + 1, **logs)
+            jsonpath = self.jsonpath.format(epoch=epoch + 1, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch + 1, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        self.model.save_weights(filepath, overwrite=True)
+                        with open(jsonpath, 'w') as f:
+                            f.write(self.model.to_json())
+                    else:
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s did not improve from %0.5f' %
+                                  (epoch + 1, self.monitor, self.best))
+            else:
+                if self.verbose > 0:

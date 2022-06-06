ddecfbc41f985c7d3f12d2fc3f162c31ec1e11d0
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-06-06 15:35:05 +0000

    File renamed

--- vnet3d.py
+++ vnet3d.py
@@ -33,3 +33,14 @@ def down_conv(x, filters, kernel_size, padding, data_format, groups):
 def up_conv_concat_conv(x, skip, filters, kernel_size, padding, strides, data_format, groups):
     channel_axis = -1 if data_format=='channels_last' else 1
     c = keras.layers.Conv3DTranspose(filters, kernel_size=(2,2,2), strides=(2,2,2), 
+                                    data_format=data_format)(x) # up dim(x) by x2
+    c = keras_contrib.layers.GroupNormalization(groups=groups, axis=channel_axis)(c)
+    c = keras.layers.Conv3D(filters, kernel_size, padding=padding, strides=strides, 
+                            activation=None, data_format=data_format)(c)
+    concat = keras.layers.Concatenate(axis=channel_axis)([c, skip]) # concat after Up; dim(skip) == 2*dim(x)
+    c = keras_contrib.layers.GroupNormalization(groups=groups, axis=channel_axis)(concat)
+    c = keras.layers.advanced_activations.PReLU()(c)
+    return c
+
+
+# Encoders

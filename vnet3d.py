57b39783eca36a4917c4ff168fd095f2ae187f7b
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-06-06 09:44:30 +0000

    Auto-update submodule pointers

--- vnet3d.py
+++ vnet3d.py
@@ -16,3 +16,20 @@ def adding_conv(x, a, filters, kernel_size, padding, strides, data_format, group
 
 def conv(x, filters, kernel_size, padding, strides, data_format, groups):
     channel_axis = -1 if data_format=='channels_last' else 1
+    c = keras.layers.Conv3D(filters, kernel_size, padding=padding, strides=strides, 
+            activation=None, data_format=data_format)(x)
+    c = keras_contrib.layers.GroupNormalization(groups=groups, axis=channel_axis)(c)
+    c = keras.layers.advanced_activations.PReLU()(c)
+    return c
+
+def down_conv(x, filters, kernel_size, padding, data_format, groups):
+    channel_axis = -1 if data_format=='channels_last' else 1
+    c = keras.layers.Conv3D(filters, kernel_size, padding=padding, strides=2, 
+                            activation=None, data_format=data_format)(x)
+    c = keras_contrib.layers.GroupNormalization(groups=groups, axis=channel_axis)(c)
+    c = keras.layers.advanced_activations.PReLU()(c)
+    return c
+
+def up_conv_concat_conv(x, skip, filters, kernel_size, padding, strides, data_format, groups):
+    channel_axis = -1 if data_format=='channels_last' else 1
+    c = keras.layers.Conv3DTranspose(filters, kernel_size=(2,2,2), strides=(2,2,2), 
